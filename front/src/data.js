1699fe240e927091d9ad157e2d9942e9685978ac
ImeneDehimi <imenedehimi8@gmail.com>
2025-10-11 17:39:53 +0100

    images in cloudinary

--- front/src/data.js
+++ front/src/data.js
@@ -4,7 +4,7 @@ const product = [
       name: 'buttermilk pancakes',
       category: 'set',
       price: 3000,
-      img: "/src/assets/image1.jpg",
+      img: "https://res.cloudinary.com/dnl4sxjlg/image/upload/v1760200633/image1_bonuvt.jpg",
       desc: "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed ",
       quantity: 1
     },
@@ -13,7 +13,7 @@ const product = [
       name: 'diner double',
       category: 'set',
       price: 3000,
-      img: '/src/assets/image2.jpg',
+      img: 'https://res.cloudinary.com/dnl4sxjlg/image/upload/v1760200596/image2_l4orxe.jpg',
       desc: "vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats ",
       quantity: 1
     },
@@ -22,7 +22,7 @@ const product = [
       name: 'godzilla milkshake',
       category: 'set',
       price: 3000,
-      img: '/src/assets/image3.jpg',
+      img: 'https://res.cloudinary.com/dnl4sxjlg/image/upload/v1760200549/image3_dbouok.jpg',
       desc: "ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.",
       quantity: 1
     },
@@ -31,7 +31,7 @@ const product = [
       name: 'country delight',
       category: 'set',
       price: 3500,
-      img: '/src/assets/image4.jpg',
+      img: 'https://res.cloudinary.com/dnl4sxjlg/image/upload/v1760200515/image4_okzsnf.jpg',
       desc: "Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, ",
       quantity: 1
     },
@@ -40,7 +40,7 @@ const product = [
       name: 'egg attack',
       category: 'set',
       price: 3000,
-      img: '/src/assets/image5.jpg',
+      img: 'https://res.cloudinary.com/dnl4sxjlg/image/upload/v1760200488/image5_yc2soq.jpg',
       desc: "franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up ",
       quantity: 1
     },
@@ -49,7 +49,7 @@ const product = [
       name: 'oreo dream',
       category: 'set',
       price: 3000,
-      img: '/src/assets/image6.jpg',
+      img: 'https://res.cloudinary.com/dnl4sxjlg/image/upload/v1760200461/image6_ab4zmf.jpg',
       desc: "Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday",
       quantity: 1
     },
@@ -58,7 +58,7 @@ const product = [
       name: 'bacon overflow',
       category: 'set',
       price: 3000,
-      img: '/src/assets/image7.jpg',
+      img: 'https://res.cloudinary.com/dnl4sxjlg/image/upload/v1760200435/image7_sfiv71.jpg',
       desc: "carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird ",
       quantity: 1
     },
@@ -67,7 +67,7 @@ const product = [
       name: 'american classic',
       category: 'set',
       price: 3000,
-      img: '/src/assets/image8.jpg',
+      img: 'https://res.cloudinary.com/dnl4sxjlg/image/upload/v1760200407/image8_f1myme.jpg',
       desc: "on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  ",
       quantity: 1
     },
@@ -76,7 +76,7 @@ const product = [
       name: 'quarantine buddy',
       category: 'ring',
       price: 1000,
-      img: "/src/assets/bague.jpg",
+      img: "https://res.cloudinary.com/dnl4sxjlg/image/upload/v1760200053/bague_z4vwxu.jpg",
       desc: "skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.",
       quantity: 1
     },
@@ -85,7 +85,7 @@ const product = [
       name: 'quarantine buddy',
       category: 'earrings',
       price: 1500,
-      img: "/src/assets/boucle.jpg",
+      img: "https://res.cloudinary.com/dnl4sxjlg/image/upload/v1760200339/boucle_h8hsgo.jpg",
       desc: "skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.",
       quantity: 1
     },
@@ -94,7 +94,7 @@ const product = [
       name: 'quarantine buddy',
       category: 'necklace',
       price: 2500,
-      img: "/src/assets/collier.jpg",
+      img: "https://res.cloudinary.com/dnl4sxjlg/image/upload/v1760200304/collier_t6vdkk.jpg",
       desc: "skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.",
       quantity: 1
     },
@@ -103,7 +103,7 @@ const product = [
       name: 'quarantine buddy',
       category: 'bracelet',
       price: 2000,
-      img: "/src/assets/bracelet.jpg",
+      img: "https://res.cloudinary.com/dnl4sxjlg/image/upload/v1760200248/bracelet_p82fxz.jpg",
       desc: "skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.",
       quantity: 1
     },
@@ -112,7 +112,7 @@ const product = [
       name: 'quarantine buddy',
       category: 'watch',
       price: 1500,
-      img: "/src/assets/montre.jpg",
+      img: "https://res.cloudinary.com/dnl4sxjlg/image/upload/v1760200141/montre_c9pyxs.png",
       desc: "skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.",
       quantity: 1
     },
